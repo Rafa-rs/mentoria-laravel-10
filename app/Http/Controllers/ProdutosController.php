aaadd5901781663053ac6921e4952ea0e4488223
Rafael Rosa <rafael.o.rosa@gmail.com>
2024-07-10 14:30:25 -0300

    Criacao de rota Produtos e Controller

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Iluminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+    protected function index()
+    {
+        return 'produto';
+    }
+}
